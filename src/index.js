bd7653e7c198874ece272efedefd64713a4ebfd7
yorchAdalber <george_nlc@hotmail.com>
2020-12-03 22:24:29 -0600

    First server testing

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const morgan = require('morgan');
 
 // Initialization
 const app = express();
@@ -10,3 +11,5 @@ const port = process.env.PORT || 3000;
 // Middlewares
 app.use(morgan('dev'));
 
+// Routes
+app.use(require('./routes/index.js'));
