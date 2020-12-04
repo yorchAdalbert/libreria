3f17499fb4c9d6724e44ba967927e23cc92283df
yorchAdalber <george_nlc@hotmail.com>
2020-12-03 22:38:00 -0600

    Add some middleware in index.js

--- src/index.js
+++ src/index.js
@@ -10,6 +10,19 @@ const port = process.env.PORT || 3000;
 
 // Middlewares
 app.use(morgan('dev'));
+app.use(express.urlencoded({extended: false});
+app.use(express.json());
 
 // Routes
 app.use(require('./routes/index.js'));
+
+// Global
+app.use((req, res, next) => {
+
+  next();
+});
+
+// Starting the server
+app.listen(port, () => {
+  console.log(`Listening on Port: ${port} `);
+});
